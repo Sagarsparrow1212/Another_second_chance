76da530f62cb93ba629a8d5eecf548294d150c81
Sagarsparrow1212 <217050457+Sagarsparrow1212@users.noreply.github.com>
2026-02-19 11:58:23 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -145,11 +145,11 @@ const staticOptions = {
   }
 };
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'), staticOptions));
-app.use(express.static(path.join(__dirname, "../dist")));
+// app.use(express.static(path.join(__dirname, "../dist")));
 
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../dist", "index.html"));
-});
+// app.use((req, res) => {
+//   res.sendFile(path.join(__dirname, "../dist", "index.html"));
+// });
 
 app.get('/health', (req, res) => {
   res.send('Hello World - Homeless App API');
